940f2357209defb3e14018bfc1f6badd4a56e16e
Gagan K. Hayer <ghayer@iteamnm.com>
2017-04-24 10:29:52 -0600

    feat(props) : add title component

--- project/src/js/client.js
+++ project/src/js/client.js
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Layout from "./pages/Layout"
 
-
+import Layout from "./components/Layout"
 
 const app = document.getElementById('app');
 
-ReactDOM.render(<Layout/>, app);
+ReactDOM.render(<Layout/>, app);   
