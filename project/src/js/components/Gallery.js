aec22bc9d10088bd8f38a26f495746c6a636de90
Gagan K. Hayer <ghayer@iteamnm.com>
2017-05-10 08:16:46 -0600

    feat(components): break large components into smaller ones

--- project/src/js/components/Gallery.js
+++ project/src/js/components/Gallery.js
@@ -27,82 +27,30 @@ class GalleryImage extends React.Component {
  }
 }
 
-// Component for gallery modal
-class GalleryModal extends React.Component {
- render() {
-  if (this.props.isOpen === false) {
-   return null;
-  }
- 
-  return(
-   <div isOpen={this.props.isOpen} className='modal-overlay' onClick={this.props.onClick} name={this.props.name}>
-    <div className='modal-body'>
-     <a className='modal-close' href='#' onClick={this.props.onClick}><span className='fa fa-times'></span></a>
- 
-     <img src={this.props.src} />
-    </div>
-   </div>
-  )
- }
-}
-
 // Component for gallery
 class Gallery extends React.Component{
- constructor(props) {
-  super(props);
- 
-  this.state = {
-   showModal: false,
-   url: ''
-  }
- 
-  this.openModal = this.openModal.bind(this);
- 
-  this.closeModal = this.closeModal.bind(this);
- }
+ constructor() {
+  super();
+}
  
  render() {
   return(
    <div refs='gallery-container' className='container-fluid gallery-container'>
    <div className='row'>
     {
-     imgUrls.map((url, index) => {
+      imgUrls.map((url, index) => {
       return <div className='col-sm-6 col-md-3 col-xl-2'>
        <div className='gallery-card'>
-        <GalleryImage className='gallery-thumbnail' src={url} alt={'Image number ' + (index + 1)} />
- 
-        <span className='card-icon-open fa fa-expand' value={url} onClick={(e) => this.openModal(url, e)}></span>
+        <GalleryImage key={index} className='gallery-thumbnail' src={url} alt={'Image number ' + (index + 1)} />
        </div>
+       <div className="card-detail"></div>
       </div>
      })
     }
    </div>
- 
-   <GalleryModal isOpen={this.state.showModal} onClick={this.closeModal} src={this.state.url} /> 
   </div>
   )
- }
- 
- // Function for opening modal dialog
- openModal(url, e) {
-  this.setState({
-   showModal: true,
-   url: url
-  })
- };
-
- // Function for closing modal dialog
- closeModal() {
-  this.setState({
-    showModal: false,
-    url: ''
-  })
  }
 }
 
 export default Gallery;
-
-// // Let's render the whole thing
-// ReactDOM.render(
-//  <Gallery imgUrls={imgUrls} />
-// , galleryContainer);
