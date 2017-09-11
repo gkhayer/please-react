ba7dbe466b6feff9e0a08578d380c7f3ee1b97e9
Gagan K. Hayer <ghayer@iteamnm.com>
2017-09-11 16:57:32 -0600

    feat(map): create map on contact page

--- project/src/js/components/Maps/Maps.js
+++ project/src/js/components/Maps/Maps.js
@@ -1,34 +1,38 @@
-// import React, {Component} from 'react';
-// import PropTypes from 'prop-types';
-// import { withGoogleMap, GoogleMap, Marker, withScriptjs } from 'react-google-maps';
-// import './Maps.css';
+import React, { Component } from 'react';
+import { withScriptjs, withGoogleMap, GoogleMap } from 'react-google-maps';
 
-// export default class Map extends Component {
-//   render() {
-//     const center = {
-//       lat: 35.188664,
-//       lng: -106.601568,
-//     };
 
-//     const GettingStartedGoogleMap = withGoogleMap(props => (
-//       <GoogleMap
-//         ref={props.onMapLoad}
-//         defaultZoom={15}
-//         defaultCenter={center}
-//       >
-//         <Marker position={center} />
-//       </GoogleMap>
-//     ));
+export default class Maps extends Component {
+  render() {
+    const center = {
+      lat: 35.188664,
+      lng: -106.601568,
+    };
+    const GettingStartedGoogleMap = withGoogleMap(props => (
+      <GoogleMap
+      ref={props.onMapLoad}
+      defaultZoom={15}
+      defaultCenter={center}
+      onClick={props.onMapClick}
+    >
+      {props.markers && props.markers.map((marker, index) => (
+        <Marker
+          {...marker}
+          onRightClick={() => props.onMarkerRightClick(index)}
+        />
+      ))}
+    </GoogleMap>
+    ));
 
-//     return (
-//       <GettingStartedGoogleMap
-//         containerElement={
-//           <div style={{ height: '100%' }} />
-//         }
-//         mapElement={
-//           <div style={{ height: '100%' }} />
-//         }
-//       />
-//     );
-//   }
-// }
+    return (
+      <GettingStartedGoogleMap
+        containerElement={
+          <div style={{ height: `100%` }} />
+        }
+        mapElement={
+          <div style={{ height: `100%` }} />
+        }
+    />
+    );
+  }
+}
