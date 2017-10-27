903c3597087b4934381b4e1fb207d2e87243c301
Gagan K. Hayer <ghayer@iteamnm.com>
2017-10-27 08:29:11 -0600

    perf(): improving the footer design

--- project/src/js/components/Footer/Footer.js
+++ project/src/js/components/Footer/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "react-router";
 
 import './Footer.css';
 
@@ -7,20 +8,29 @@ export default class Footer extends React.Component {
     return (
       <section>
         <footer className="mdl-mega-footer">
-          <div className="mdl-mega-footer__bottom-section">
-            <div className="mdl-logo">Features</div>
-            <ul className="mdl-mega-footer__link-list">
-              <li><a href="#">About</a></li>
-              <li><a href="#">Terms</a></li>
-              <li><a href="#">Partners</a></li>
-              <li><a href="#">Updates</a></li>
-            </ul>
+          <div className="mdl-mega-footer__middle-section">
+            <div className="mdl-mega-footer__drop-down-section">
+              <h1 className="mdl-mega-footer__heading">Features</h1>
+              <ul className="mdl-mega-footer__link-list">
+                <li><Link to="About">About</Link></li>
+                <li><Link to="#">Terms</Link></li>
+                <li><Link to="Partners">Partners</Link></li>
+              </ul>
+            </div>
+            <div className="mdl-mega-footer__drop-down-section">
+              <h1 className="mdl-mega-footer__heading">FAQ</h1>
+              <ul className="mdl-mega-footer__link-list">
+                <li><Link to="#">Questions</Link></li>
+                <li><Link to="#">Answers</Link></li>
+                <li><Link to="Contact">Contact us</Link></li>
+              </ul>
+            </div>
           </div>
           <div className="mdl-mega-footer__bottom-section">
             <div className="mdl-logo">Title</div>
             <ul className="mdl-mega-footer__link-list">
-              <li><a href="#">Help</a></li>
-              <li><a href="#">Privacy & Terms</a></li>
+              <li><Link to="#">Help</Link></li>
+              <li><Link to="#">Privacy & Terms</Link></li>
             </ul>
           </div>
         </footer>
