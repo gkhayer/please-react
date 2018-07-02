e35b67ab279d9b767b8994a08f565c6d9e7eaf06
Gagan K. Hayer <ghayer@iteamnm.com>
2018-07-02 17:27:54 -0600

    fix(Gallery): fix responsiveness of gallery items

--- src/js/components/Gallery/Gallery.js
+++ src/js/components/Gallery/Gallery.js
@@ -32,53 +32,55 @@ export default class Gallery extends React.Component{
   const selectedItem = this.state.selectedItem;
 
   return(
-    <div className='gallery'>
-      {
-        cardDetail.map((value, idx) => {
-          return (
-          <div key={`img${idx}`} className='gallery-card'>
-            <img className="images-card" src={value.image} alt={value.itemName}/>
-            <div className="card-detail">
-              <span className="item-price">{value.itemPrice}</span>
-              <span>{value.itemName}</span>
-              <Button
-                bsStyle="primary"
-                bsSize="small"
-                onClick={this.open(value).bind(this)} >
-                View Detail
-              </Button>
+    <div className="container">
+      <h1>Featured Products</h1>
+      <div className='gallery'>
+        {
+          cardDetail.slice(0, 3).map((value, idx) => {
+            return (
+            <div key={`img${idx}`} className='gallery-card'>
+              <img className="images-card" src={value.image} alt={value.itemName}/>
+              <div className="card-detail">
+                <span className="item-price">{value.itemPrice}<span>{value.itemName}</span></span>
+                <Button
+                  bsStyle="primary"
+                  bsSize="small"
+                  onClick={this.open(value).bind(this)} >
+                  View Detail
+                </Button>
+              </div>
             </div>
-          </div>
-        )})
-      }
-      { selectedItem &&
-        <Modal show onHide={this.close.bind(this)}>
-          <Modal.Header closeButton>
-            <Modal.Title >{selectedItem.itemName}</Modal.Title>
-            {selectedItem.itemDetail}
-          </Modal.Header>
-          <Modal.Body>
-            <img src={selectedItem.image} alt={selectedItem.name}/>
-            <div className="input-quantity">
-            <div className="input-group">
-            <input type="number" className="input-control" placeholder="Qty" aria-describedby="basic-addon1" />
-          </div>
-          <div className="btn-group">
-            <button className="btn btn-warning addcart"
-              type="button"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-            Add to Cart
-            </button>
-          </div>
+          )})
+        }
+        { selectedItem &&
+          <Modal show onHide={this.close.bind(this)}>
+            <Modal.Header closeButton>
+              <Modal.Title >{selectedItem.itemName}</Modal.Title>
+              {selectedItem.itemDetail}
+            </Modal.Header>
+            <Modal.Body>
+              <img src={selectedItem.image} alt={selectedItem.name}/>
+              <div className="input-quantity">
+              <div className="input-group">
+              <input type="number" className="input-control" placeholder="Qty" aria-describedby="basic-addon1" />
             </div>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button onClick={this.close.bind(this)}>Close</Button>
-          </Modal.Footer>
-        </Modal>
-      }
+            <div className="btn-group">
+              <button className="btn btn-warning addcart"
+                type="button"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+              Add to Cart
+              </button>
+            </div>
+              </div>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button onClick={this.close.bind(this)}>Close</Button>
+            </Modal.Footer>
+          </Modal>
+        }
+      </div>
     </div>
   )
   }
