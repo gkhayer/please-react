3e5956993d876f964308821d1ed2077a6353d37f
Gagan K. Hayer <ghayer@iteamnm.com>
2018-07-02 17:29:18 -0600

    style(Gaming): delete height and width from img

--- src/js/pages/Gaming/Gaming.js
+++ src/js/pages/Gaming/Gaming.js
@@ -4,6 +4,7 @@ import { Row, Col, Grid, Carousel, Nav, NavItem} from 'react-bootstrap';
 import './Gaming.css';
 
 import VerticalNav from '../../components/VerticalNav';
+import Gallery from '../../components/Gallery';
 
 export default class Gaming extends React.Component {
 
@@ -13,23 +14,24 @@ export default class Gaming extends React.Component {
         <div className="gaming-carousel">
           <Carousel>
             <Carousel.Item>
-              <img width={1200} height={500} alt="gaming" src="../../Images/gaming/game1.jpg"/>
+              <img alt="gaming" src="../../Images/gaming/game1.jpg"/>
             </Carousel.Item>
             <Carousel.Item>
-              <img width={1200} height={500} alt="gaming" src="../../Images/gaming/game2.jpg"/>
+              <img alt="gaming" src="../../Images/gaming/game2.jpg"/>
             </Carousel.Item>
             <Carousel.Item>
-              <img width={1200} height={500} alt="gaming" src="../../Images/gaming/game3.jpg"/>
+              <img alt="gaming" src="../../Images/gaming/game3.jpg"/>
             </Carousel.Item>
             <Carousel.Item>
-              <img width={1200} height={500} alt="gaming" src="../../Images/gaming/game4.jpg"/>
+              <img alt="gaming" src="../../Images/gaming/game4.jpg"/>
             </Carousel.Item>
             <Carousel.Item>
-              <img width={1200} height={500} alt="gaming" src="../../Images/gaming/game5.jpg"/>
+              <img alt="gaming" src="../../Images/gaming/game5.jpg"/>
             </Carousel.Item>
           </Carousel>
         </div>
         <VerticalNav />
+        <Gallery />
       </div>
     )
   }
